8ee336686c2c40ad1a750f62c2c519d03060344e
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-27 12:41:06 -0700

    add client and refactor

--- play.js
+++ play.js
@@ -1,27 +1,28 @@
-const net = require("net");
+const connect = require('./client')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    port: 50541, // PORT number here,
-    host: "165.227.47.243" // IP address here,
-  });
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     port: 50541, // PORT number here,
+//     host: "165.227.47.243" // IP address here,
+//   });
 
-  // Event Handler for connect
-  conn.on("connect", (data) => { 
-    console.log("Successful connection to snek");
-  });
+//   // Event Handler for connect
+//   conn.on("connect", (data) => { 
+//     console.log("Successful connection to snek");
+//     conn.write("snekky snek")
+//   });
 
-  // Event Handler for getting booted
-  conn.on("data", (data) => {
-    console.log("you ded cuz you idled");
-  });
+//   // Event Handler for getting booted
+//   conn.on("data", (data) => {
+//     console.log("you ded cuz you idled");
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
